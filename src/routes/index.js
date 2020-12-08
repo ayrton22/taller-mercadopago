89d00786fe0ad72228cc737e25340ea36680399e
Ayrton <themasterayrx@hotmail.com>
2020-12-08 00:11:22 -0300

    push para heroku 2

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const indexController = require("../controllers/indexController");
+
+/* GET home page. */
+router.get("/", indexController.home);
+
+/* GET detail page */
+router.get("/detail", indexController.detail);
+
+router.post("/comprar", indexController.comprar);
+
+router.get("/callback", indexController.callback);
+
+router.post("/notifications", indexController.notifications);
+
+module.exports = router;
